243275c92a1f8ba983904f472b9cd9f27fe17861
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-01-14 23:05:01 +0530

    connected to the DB

--- app.js
+++ app.js
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware');
   next();
 });
 
--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const dotenv = require('dotenv').config();
-const app = require('./app.js');
+require('dotenv').config();
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require('mongoose');
+const app = require('./app');
+
+const DB = process.env.DATABASE_CONNECTION_STRING.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('DB connection successfull');
+  });
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
