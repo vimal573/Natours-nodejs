c2721630cd45032829449423a6891e7ac9a72690
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-01-18 10:00:23 +0530

    crate user model and signup

---
+++ models/userModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please provide a name']
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowerCase: true,
+    validate: [validator.isEmail, 'Please provide a vlid email']
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minLength: [8, 'Password must be equal or more then 8 cahracters'],
+    select: false
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on CREATE and SAVE!!!
+      validator: function(el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!'
+    }
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const userController = require('../controller/userController');
+const authController = require('../controller/authController');
 
 const router = express.Router();
+
+router.post('/signup', authController.signUp);
 
 router
   .route('/')
