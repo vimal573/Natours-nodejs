a4e08c960c16849ea38c781c2236eab8706b919f
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-01-25 01:01:21 +0530

    Prevent parameter polution

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globelErrorHandler = require('./controller/errorController');
@@ -43,6 +44,20 @@ app.use(mongoSanitize());
 
 // Data sanitization against xss
 app.use(xss());
+
+// Prevent Parameter polution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price'
+    ]
+  })
+);
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
