01777c135ff315ec81f18f203031c86fa02762b9
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-01-25 00:47:17 +0530

    Data snitization

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 /* eslint-disable import/no-extraneous-dependencies */
+
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globelErrorHandler = require('./controller/errorController');
@@ -34,6 +37,12 @@ app.use(
     limit: '10kb'
   })
 );
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against xss
+app.use(xss());
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`));
