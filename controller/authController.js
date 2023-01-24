45a7f691639545bdcdf4b95af7eb3a9838b44089
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-01-25 00:04:17 +0530

    AuthController - sending JWT via cookie

--- controller/authController.js
+++ controller/authController.js
@@ -13,14 +13,28 @@ const signToken = id => {
 };
 
 const createSendToken = (user, statusCode, res) => {
-  user.password = undefined;
-
   const token = signToken(user.id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: 'success',
     token,
-    user
+    data: {
+      user
+    }
   });
 };
 
