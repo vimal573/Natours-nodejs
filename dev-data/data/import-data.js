7e4950bc99948bb376d97b107eb19f28954813ae
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-01-28 06:58:26 +0530

    adding missing authentication and authorization

--- dev-data/data/import-data.js
+++ dev-data/data/import-data.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
 const Tour = require('../../models/tourModel');
+const Review = require('../../models/reviewModel');
+const User = require('../../models/userModel');
 
 const DB = process.env.DATABASE_CONNECTION_STRING.replace(
   '<PASSWORD>',
@@ -19,10 +21,16 @@ mongoose
   });
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf-8'));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, 'utf-8')
+);
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf-8'));
 
 const importData = async (req, res) => {
   try {
     await Tour.create(tours);
+    await Review.create(reviews);
+    await User.create(users, { validateBeforeSave: false });
     console.log('Data successfully imported');
   } catch (error) {
     console.log(error);
@@ -33,6 +41,8 @@ const importData = async (req, res) => {
 const deleteData = async (req, res) => {
   try {
     await Tour.deleteMany();
+    await Review.deleteMany();
+    await User.deleteMany();
     console.log('Data successfully deleted');
   } catch (error) {
     console.log(error);
