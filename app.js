41eadcbdc6038483b4e19f32e083e40b065d1001
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-01-29 20:24:20 +0530

    Extending our base template with blocks

--- app.js
+++ app.js
@@ -72,6 +72,20 @@ app.use((req, res, next) => {
   next();
 });
 
+// 3 Routes
+
+app.use('/overview', (req, res, next) => {
+  res.status(200).render('overview', {
+    title: 'All Tours'
+  });
+});
+
+app.use('/tour', (req, res, next) => {
+  res.status(200).render('tour', {
+    title: 'The Forest Hiker Tour'
+  });
+});
+
 app.use('/', (req, res, next) => {
   res.status(200).render('base', {
     tour: 'The Forest Hiker',
@@ -79,7 +93,6 @@ app.use('/', (req, res, next) => {
   });
 });
 
-// 3 Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
