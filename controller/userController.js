d64ad96bb40bf748eaa8e0dd1ab91c3bee1181f6
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-02-03 08:26:29 +0530

    Configuring multer

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,35 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const multer = require('multer');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./../controller/handlerFactory');
+
+const multerStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/img/users');
+  },
+  filename: (req, file, cb) => {
+    // user-id-curtimestamp.jpg
+    const ext = file.mimetype.split('/')[1];
+    cb(null, `user-${req.user.id}-${Date.now()}.${ext}`);
+  }
+});
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only image.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter
+});
+
+exports.uploadUserPhoto = upload.single('photo');
 
 const filterObj = (obj, ...allowedFields) => {
   const newObj = {};
@@ -18,6 +46,9 @@ exports.getMe = (req, res, next) => {
 };
 
 exports.updateMe = catchAsync(async (req, res, next) => {
+  console.log(req.body);
+  console.log(req.file);
+
   // 1) Create error if user posts password data
   if (req.body.password || req.body.confirmPassword) {
     return next(
